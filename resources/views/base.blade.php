eb81cbaacba4c71378dfee106e64cc3ea7338a3e
Yoga838 <212410102031@mail.unej.ac.id>
2024-06-24 07:49:35 +0700

    delete unused logo

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -62,8 +62,7 @@
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
-      <img src="dist/img/AdminLTELogo.png" alt="AdminLTE Logo" class="brand-image img-circle elevation-3" style="opacity: .8">
+    <a href="{{ route('dashboard') }}" class="brand-link">
       <span class="brand-text font-weight-light">Master Cell</span>
     </a>
 
